118ad33609f63e3b007a55d7a23e2af073238d8c
mtengbot <eng.bot@moderntreasury.com>
2026-07-23 15:44:59 +0000

    seal

--- tests/api-resources/payment-orders/reversals.test.ts
+++ tests/api-resources/payment-orders/reversals.test.ts
@@ -9,6 +9,28 @@ const client = new ModernTreasury({
 });
 
 describe('resource reversals', () => {
+  test('list', async () => {
+    const responsePromise = client.paymentOrders.reversals.list('payment_order_id');
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  test('list: request options and params are passed correctly', async () => {
+    // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
+    await expect(
+      client.paymentOrders.reversals.list(
+        'payment_order_id',
+        { after_cursor: 'after_cursor', per_page: 0 },
+        { path: '/_stainless_unknown_path' },
+      ),
+    ).rejects.toThrow(ModernTreasury.NotFoundError);
+  });
+
   test('create: only required params', async () => {
     const responsePromise = client.paymentOrders.reversals.create('payment_order_id', {
       reason: 'duplicate',
@@ -83,27 +105,5 @@ describe('resource reversals', () => {
     const response = await client.paymentOrders.reversals.retrieve('reversal_id', {
       payment_order_id: 'payment_order_id',
     });
-  });
-
-  test('list', async () => {
-    const responsePromise = client.paymentOrders.reversals.list('payment_order_id');
-    const rawResponse = await responsePromise.asResponse();
-    expect(rawResponse).toBeInstanceOf(Response);
-    const response = await responsePromise;
-    expect(response).not.toBeInstanceOf(Response);
-    const dataAndResponse = await responsePromise.withResponse();
-    expect(dataAndResponse.data).toBe(response);
-    expect(dataAndResponse.response).toBe(rawResponse);
-  });
-
-  test('list: request options and params are passed correctly', async () => {
-    // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
-    await expect(
-      client.paymentOrders.reversals.list(
-        'payment_order_id',
-        { after_cursor: 'after_cursor', per_page: 0 },
-        { path: '/_stainless_unknown_path' },
-      ),
-    ).rejects.toThrow(ModernTreasury.NotFoundError);
   });
 });
