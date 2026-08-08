a669ccc3124a52b0cf1168b610115da786a4e192
mtengbot <eng.bot@moderntreasury.com>
2026-08-08 04:11:22 +0000

    chore: regenerate SDKs from f155d2510aff4f58fe615eecbea6bd7c3ab7189b
    Stainless-Generated-From: 4f29dcf56c5a0c238d0311a1d2de56ae46d1d5e5

--- tests/api-resources/internal-accounts/internal-accounts.test.ts
+++ tests/api-resources/internal-accounts/internal-accounts.test.ts
@@ -86,7 +86,7 @@ describe('resource internalAccounts', () => {
         line2: 'line2',
       },
       party_name: 'party_name',
-      requested_account_number_types: ['ethereum_address'],
+      requested_account_number_types: ['base_address'],
       vendor_attributes: {
         key: 'value',
         foo: 'bar',
