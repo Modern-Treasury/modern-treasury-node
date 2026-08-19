4d4ed55d4bc92f3c25a7c36e5e34ffe1ab2f487e
mtengbot <eng.bot@moderntreasury.com>
2026-08-19 17:08:22 +0000

    seal

--- src/resources/internal-accounts/balance-reports.ts
+++ src/resources/internal-accounts/balance-reports.ts
@@ -226,9 +226,8 @@ export namespace BalanceReport {
 
     /**
      * The type of `vendor_code` being reported. Can be one of `bai2`, `bankprov`,
-     * `bnk_dev`, `cleartouch`, `currencycloud`, `cross_river`, `dc_bank`, `dwolla`,
-     * `evolve`, `goldman_sachs`, `iso20022`, `jpmc`, `mx`, `silvergate`, `swift`, or
-     * `us_bank`.
+     * `bnk_dev`, `cleartouch`, `currencycloud`, `cross_river`, `dwolla`, `evolve`,
+     * `goldman_sachs`, `iso20022`, `jpmc`, `mx`, `silvergate`, `swift`, or `us_bank`.
      */
     vendor_code_type: string | null;
   }
@@ -303,9 +302,8 @@ export namespace BalanceReportCreateParams {
 
     /**
      * The type of `vendor_code` being reported. Can be one of `bai2`, `bankprov`,
-     * `bnk_dev`, `cleartouch`, `currencycloud`, `cross_river`, `dc_bank`, `dwolla`,
-     * `evolve`, `goldman_sachs`, `iso20022`, `jpmc`, `mx`, `silvergate`, `swift`, or
-     * `us_bank`.
+     * `bnk_dev`, `cleartouch`, `currencycloud`, `cross_river`, `dwolla`, `evolve`,
+     * `goldman_sachs`, `iso20022`, `jpmc`, `mx`, `silvergate`, `swift`, or `us_bank`.
      */
     vendor_code_type: string | null;
 
