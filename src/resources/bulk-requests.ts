0598870829cbc263e4a4ce4f8e1bd01c167f4b86
mtengbot <eng.bot@moderntreasury.com>
2026-08-26 20:10:08 +0000

    Build SDK
    Stainless-Generated-From: 612d2f111d496ce53c1c72931c520df12ab995b3

--- src/resources/bulk-requests.ts
+++ src/resources/bulk-requests.ts
@@ -341,10 +341,9 @@ export namespace BulkRequestCreateParams {
 
     /**
      * If present, this will replace your default company name on receiver's bank
-     * statement. For ACH, only the first 16 characters of this string will be used.
-     * This must be a name the account holder legally operates under, such as a DBA. Do
-     * not use it to originate on behalf of another person or business, including an
-     * end customer.
+     * statement. This field can only be used for ACH payments currently. For ACH, only
+     * the first 16 characters of this string will be used. Any additional characters
+     * will be truncated.
      */
     originating_party_name?: string | null;
 
@@ -1109,10 +1108,9 @@ export namespace BulkRequestCreateParams {
 
     /**
      * If present, this will replace your default company name on receiver's bank
-     * statement. For ACH, only the first 16 characters of this string will be used.
-     * This must be a name the account holder legally operates under, such as a DBA. Do
-     * not use it to originate on behalf of another person or business, including an
-     * end customer.
+     * statement. This field can only be used for ACH payments currently. For ACH, only
+     * the first 16 characters of this string will be used. Any additional characters
+     * will be truncated.
      */
     originating_party_name?: string | null;
 
