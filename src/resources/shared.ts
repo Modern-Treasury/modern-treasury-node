9b067c6224445e4e9c8b3c01269a7e9d0ed238f0
mtengbot <eng.bot@moderntreasury.com>
2026-08-28 16:14:55 +0000

    chore: regenerate SDKs from 648842f54e4bd55a71ffbacf5c3f8bbc47cb77df
    Stainless-Generated-From: 14448b60b86567bc2b309bc15d0816eb255dd69d

--- src/resources/shared.ts
+++ src/resources/shared.ts
@@ -94,6 +94,11 @@ export interface ChildLegalEntityCreate {
   business_description?: string | null;
 
   /**
+   * Legal designation associated with the business.
+   */
+  business_designation?: 'exempt_financial_institution' | 'non_operating_business' | null;
+
+  /**
    * The business's legal business name.
    */
   business_name?: string | null;
